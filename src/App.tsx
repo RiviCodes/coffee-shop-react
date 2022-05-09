dcd877fe897cae09e9a9a8b9a6457e08bff2b669
RiviCodes <jose.riveramz@udlap.mx>
2022-05-09 01:10:08 -0500

    Render Intro and Services components into the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Intro from "./components/Intro";
+import Services from "./components/Services";
 import "./App.css";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <>
       <Header />
       <Intro />
+      <Services />
     </>
   );
 }
