d6765472034eb05ba948dc46c31ca3c7d9ea8768
RiviCodes <jose.riveramz@udlap.mx>
2022-05-09 00:41:46 -0500

    Initiate App as TSX file

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Intro from "./components/Intro";
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Intro />
+    </>
+  );
+}
