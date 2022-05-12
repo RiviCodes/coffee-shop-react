d082ea71768ca61ec15db89bb210e75a6937dbac
RiviCodes <jose.riveramz@udlap.mx>
2022-05-11 21:14:45 -0500

    Rename Header component to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "../styles/Header.css";
+import "../styles/Navbar.css";
 
-export default function Header() {
+export default function Navbar() {
   return (
     <>
       <section id="header-section">
