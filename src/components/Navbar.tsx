c4843e88c7a5f3ddd59da1c35d334e978df1a789
RiviCodes <jose.riveramz@udlap.mx>
2022-05-12 14:51:18 -0500

    Rename ID's & Classes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,36 +3,34 @@ import "../styles/Navbar.css";
 
 export default function Navbar() {
   return (
-    <>
-      <section id="header-section">
-        <div className="header--left_side">
-          <img
-            src="./images/header/coffee-beans.png"
-            alt="Beans Logo"
-            className="header--beans_logo"
-          />
-          <div id="header-logo">COFFEE SHOP</div>
-        </div>
-        <div className="header--right_side">
-          <ul id="header-list">
-            <li className="header--li_item">
-              <a className="header-anchor" href="test">
-                LINK ONE
-              </a>
-            </li>
-            <li className="header--li_item">
-              <a className="header-anchor" href="test">
-                LINK TWO
-              </a>
-            </li>
-            <li className="header--li_item">
-              <a className="header-anchor" href="test">
-                ABOUT US
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-    </>
+    <section id="navbar-section">
+      <div className="navbar--left_side">
+        <img
+          src="./images/header/coffee-beans.png"
+          alt="Beans Logo"
+          className="navbar--beans_logo"
+        />
+        <div id="navbar-logo">COFFEE SHOP</div>
+      </div>
+      <div className="navbar--right_side">
+        <ul id="navbar-list">
+          <li className="navbar--li_item">
+            <a className="navbar-anchor" href="test">
+              LINK ONE
+            </a>
+          </li>
+          <li className="navbar--li_item">
+            <a className="navbar-anchor" href="test">
+              LINK TWO
+            </a>
+          </li>
+          <li className="navbar--li_item">
+            <a className="navbar-anchor" href="test">
+              ABOUT US
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 }
