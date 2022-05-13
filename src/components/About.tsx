5fc807cc01597427f727477da2caa6deb7d6dd6c
RiviCodes <jose.riveramz@udlap.mx>
2022-05-12 19:51:49 -0500

    Add classes, fix containers

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,22 +4,22 @@ import "../styles/About.css";
 export default function About() {
   return (
     <>
-      <section id="about-our-coffee" className="grid-about">
-        <div id="water">
+      <section id="about-section" className="grid-about">
+        <div className="about-left_side">
           <h3>
             About our <span>Coffee</span>
           </h3>
-          <p>
+          <p className="about-p">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur
             dolor voluptatem nobis, illum dignissimos possimus error porro culpa
             aperiam iste quis atque libero ipsum enim sequi optio beatae
             delectus nesciunt.
           </p>
-
           <div>
             <img
               src="images/about-our-coffee/coffee-cup.png"
               alt="100% Natural Coffee"
+              className="about-icon"
             />
             <div>
               <h3>100% Natural Coffee</h3>
@@ -33,6 +33,7 @@ export default function About() {
             <img
               src="images/about-our-coffee/heart.png"
               alt="Quality product"
+              className="about-icon"
             />
             <div>
               <h3>Quality product</h3>
@@ -46,6 +47,7 @@ export default function About() {
             <img
               src="images/about-our-coffee/shopping-cart.png"
               alt="Free delivery"
+              className="about-icon"
             />
             <div>
               <h3>Free delivery</h3>
@@ -56,11 +58,19 @@ export default function About() {
             </div>
           </div>
         </div>
+        <div className="about-right_side">
+          <img
+            src="images/about-our-coffee/our-coffee-1.jpg"
+            alt="Our Coffee"
+            className="about-picture"
+          />
+          <img
+            src="images/about-our-coffee/our-coffee-2.jpg"
+            alt="Our Coffee"
+            className="about-picture"
+          />
+        </div>
       </section>
-      <div id="water2">
-        <img src="images/about-our-coffee/our-coffee-1.jpg" alt="Our Coffee" />
-        <img src="images/about-our-coffee/our-coffee-2.jpg" alt="Our Coffee" />
-      </div>
     </>
   );
 }
