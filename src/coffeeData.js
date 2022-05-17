e6c1f442edbfb5879bf5d58739d5efa37bc30ef4
RiviCodes <jose.riveramz@udlap.mx>
2022-05-17 15:41:59 -0500

    Add JS file to test info received

---
+++ src/coffeeData.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    id: 1,
+    title: "Light roast",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat laborum aut magnam harum veritatis.",
+    flavors: ["Pronounced acidity", "Origin flavors predominate"],
+    imageUrl: "./images/products/light-roast.png",
+  },
+];
