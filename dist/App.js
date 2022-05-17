32eacd518e72304efbe22a3fb6451a89fbb97b29
RiviCodes <jose.riveramz@udlap.mx>
2022-05-17 15:43:01 -0500

    Compile JS file

--- dist/App.js
+++ dist/App.js
@@ -7,9 +7,10 @@ const jsx_runtime_1 = require("react/jsx-runtime");
 const Navbar_1 = __importDefault(require("./components/Navbar"));
 const Intro_1 = __importDefault(require("./components/Intro"));
 const Services_1 = __importDefault(require("./components/Services"));
-/* import About from "./components/About"; */
+const About_1 = __importDefault(require("./components/About"));
+const Products_1 = __importDefault(require("./components/Products"));
 require("./App.css");
 function App() {
-    return ((0, jsx_runtime_1.jsxs)(jsx_runtime_1.Fragment, { children: [(0, jsx_runtime_1.jsx)(Navbar_1.default, {}), (0, jsx_runtime_1.jsx)(Intro_1.default, {}), (0, jsx_runtime_1.jsx)(Services_1.default, {})] }));
+    return ((0, jsx_runtime_1.jsxs)(jsx_runtime_1.Fragment, { children: [(0, jsx_runtime_1.jsx)(Navbar_1.default, {}), (0, jsx_runtime_1.jsx)(Intro_1.default, {}), (0, jsx_runtime_1.jsx)(Services_1.default, {}), (0, jsx_runtime_1.jsx)(About_1.default, {}), (0, jsx_runtime_1.jsx)(Products_1.default, {})] }));
 }
 exports.default = App;
